20236f05bd36b15178020584eea9d7b7569550a4
robocop019 <chrisbowen@comcast.net>
2019-05-07 16:29:21 -0500

    added partial view

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime("%b%e, %Y,%l:%M %p")
+  end
+
+  def full_name
+    "#{first_name} #{middle_name} #{last_name}"
+  end
 end
